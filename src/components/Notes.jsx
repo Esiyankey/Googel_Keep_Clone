8f211fe8abf0e134a7f6ee92e6684a592e936bbb
Esi Yankey <graceyan244@gmail.com>
2023-05-22 15:52:35 +0000

    fix:fix bugs

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BiImage, IoIosCheckboxOutline, AiOutlineBulb } from "react-icons/all";
+import { BiImage, IoIosCheckboxOutline, AiOutlineBulb ,IoColorPaletteOutline, RiInboxArchiveLine,BsThreeDotsVertical,BsPin} from "react-icons/all";
 export const Notes = () => {
   const [isFocused, setIsFocused] = useState(false);
 
@@ -9,24 +9,59 @@ export const Notes = () => {
 
   const handleBlur = () => {
     setIsFocused(false);
+    setNote("");
+    setTitle("")
   };
+  const handleInputChange = (e) => {
+    if (e.target.id === 'titleInput') {
+      setTitle(e.target.value);
+    } else if (e.target.id === 'noteText') {
+      setNote(e.target.value);
+    }
+  
+  };
+  const [title, setTitle] = useState('');
+  const [note, setNote] = useState('');
   return (
     <>
       <div className="take-notes">
         <div className="NotesInput">
+          <div className="conatainer">
           <input
             type="text"
             className="input "
             onFocus={handleFocus}
-            onBlur={handleBlur}
             placeholder="Take a note"
           />
-                    {/* {isFocused && (
+                    {isFocused && (
                   <>
-                    <input type="text" className="additional-input" placeholder="Text" />
-                    
+                  <div className="note-popup">
+                    <div className="input-pin">
+                    <input type="text" className="title" placeholder="Title" 
+                    value={title}
+                    id="titleInput"
+                    onChange={handleInputChange}/>
+                    <BsPin className="pin"/>
+                    </div>
+                    <input type="text" id="noteText" className="note" placeholder="Take a note"
+                    value={note}
+                    onChange={handleInputChange} />
+
+                    <div className="popup-icons">
+                        <div className="popup-icon">
+                          <BiImage/>
+                          <IoColorPaletteOutline/>
+                          < RiInboxArchiveLine/>
+                          <BsThreeDotsVertical/>
+                        </div>
+                        <button className="button" onClick={handleBlur}>
+                          close
+                        </button>
+                    </div>
+                  </div>
                   </>
-                )} */}
+                )}
+          </div>
           <div className="inputIcons">
             <IoIosCheckboxOutline className="input-icons" />
             <BiImage className="input-icons" />
@@ -35,7 +70,7 @@ export const Notes = () => {
       </div>
         <div className="backgroundImage">
           <AiOutlineBulb className="outline" />
-          <h4>Notes you add appear here</h4>
+          <h5>Notes you add appear here</h5>
         </div>
     </>
   );
